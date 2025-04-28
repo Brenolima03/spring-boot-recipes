670f793399ac3fa1fa0e9ec37ec39a2f63d6ef5f
Brenolima03 <brenocslima@outlook.com>
2025-04-28 17:31:50 -0400

    first commit

---
+++ src/test/java/com/br/recipes/RecipesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.br.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
