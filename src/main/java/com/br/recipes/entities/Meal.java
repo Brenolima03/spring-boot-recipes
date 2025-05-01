f967d76f8b80b9326173f0cbba326e594d952298
Brenolima03 <brenocslima@outlook.com>
2025-05-01 14:43:14 -0400

    changing the type of image to text in the database

--- src/main/java/com/br/recipes/entities/Meal.java
+++ src/main/java/com/br/recipes/entities/Meal.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import com.br.recipes.entities.dto.MealRequestDTO;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,7 @@ public class Meal {
   @GeneratedValue(strategy=GenerationType.UUID)
   private UUID id;
   private String name;
+  @Column(columnDefinition = "TEXT")
   private String image;
   private Float price;
 
