cb0547a522fe4ba6721866c3f8be3983fe671481
Brenolima03 <brenocslima@outlook.com>
2025-05-05 14:01:27 -0400

    removing the cors configuration from the main file

--- src/main/java/com/br/recipes/RecipesApplication.java
+++ src/main/java/com/br/recipes/RecipesApplication.java
@@ -1,28 +1,11 @@
 package com.br.recipes;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RecipesApplication {
   public static void main(String[] args) {
     SpringApplication.run(RecipesApplication.class, args);
-  }
-
-  @Value("${app.cors.allowed-origins}")
-  private String allowedOrigin;
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(allowedOrigin);
-      }
-    };
   }
 }
