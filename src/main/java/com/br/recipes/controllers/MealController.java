a7cca60c335763ebb2168524edb7fdb5bb943c7d
Brenolima03 <brenocslima@outlook.com>
2025-05-26 15:32:52 -0400

    applying hateoas logic to the meal controller

--- src/main/java/com/br/recipes/controllers/MealController.java
+++ src/main/java/com/br/recipes/controllers/MealController.java
@@ -1,9 +1,17 @@
 package com.br.recipes.controllers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.br.recipes.entities.Meal;
 import com.br.recipes.entities.dto.MealRequestDTO;
 import com.br.recipes.entities.dto.MealResponseDTO;
+import com.br.recipes.exceptions.MealNotFoundException;
 import com.br.recipes.repositories.MealRepository;
 
 @RestController
@@ -26,10 +35,32 @@ public class MealController {
     mealRepository.save(meal);
   }
 
-  @GetMapping
-  public List<MealResponseDTO> getAll() {
-    List<MealResponseDTO> mealList =
-      mealRepository.findAll().stream().map(MealResponseDTO::new).toList();
-    return mealList;
+  @GetMapping()
+  public CollectionModel<EntityModel<MealResponseDTO>> findAll() {
+    List<EntityModel<MealResponseDTO>> meals = mealRepository.findAll().stream()
+      .map(meal -> {
+        MealResponseDTO dto = new MealResponseDTO(meal);
+        return EntityModel.of(dto,
+          linkTo(methodOn(MealController.class)
+            .findOne(meal.getId())).withSelfRel(),
+          linkTo(methodOn(MealController.class).findAll()).withRel("meals"));
+      })
+      .collect(Collectors.toList());
+
+    return CollectionModel.of(
+      meals, linkTo(methodOn(MealController.class).findAll()).withSelfRel()
+    );
+  }
+
+  @GetMapping("/{id}")
+  public EntityModel<MealResponseDTO> findOne(@PathVariable UUID id) {
+    Meal meal = mealRepository.findById(id)
+      .orElseThrow(() -> new MealNotFoundException(id));
+
+    MealResponseDTO dto = new MealResponseDTO(meal);
+
+    return EntityModel.of(dto,
+      linkTo(methodOn(MealController.class).findOne(id)).withSelfRel(),
+      linkTo(methodOn(MealController.class).findAll()).withRel("meals"));
   }
 }
